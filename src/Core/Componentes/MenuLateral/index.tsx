71d5bd2efad53fc377197728d50856d61fd39415
KATO1975 <crlsndrsgtrrzhrnndz@gmail.com>
2025-05-06 10:25:03 -0500

    se monatron iconos de menu lateral y se aplicaron estilos

--- src/Core/Componentes/MenuLateral/index.tsx
+++ src/Core/Componentes/MenuLateral/index.tsx
@@ -1,81 +1,138 @@
+import { useNavigate } from "react-router-dom";
+import "./Style.css";
 
-import { useNavigate } from 'react-router-dom';
-import './Style.css';
+const Munu = () => {
+  const navigate = useNavigate(); // Hook para navegar entre rutas
 
+  const handleRegistro = () => {
+    navigate("/registro"); // Navega a la ruta de registro
+  };
 
-const Munu =() => {
+  const RegistroDegatos = () => {
+    navigate("/regitroAnimal"); // Navega a la ruta de regitroAnimal
+  };
 
-    const navigate = useNavigate(); // Hook para navegar entre rutas
+  const Citas = () => {
+    navigate("/regitroCitas"); // Navega a la ruta de regitroCitas
+  };
 
-    const handleRegistro = () => {
-        navigate('/registro'); // Navega a la ruta de registro
-    };
+  const Tratamientos = () => {
+    navigate("/regitroTratamiento"); // Navega a la ruta de regitroTratamiento
+  };
 
-    const RegistroDegatos = () => {
-        navigate('/regitroAnimal'); // Navega a la ruta de registro
-    };
+  const Mensajes = () => {
+    navigate("/mensaje"); // Navega a la ruta de mensaje
+  };
 
+  const Cerrarsesión = () => {
+    navigate("/login"); // Navega a la ruta de login
+  };
 
-    return(
-        <div className='contenedor-principal'>
-        <div className='contenedor-iconos'>
-            <div className='contenedor0'>
-                <img src="src/assets/Vector 1.svg" alt="" />
-            </div>
-
-            <div className='contenedor1'>
-                <p className='titulo1'>
-                    Peponé
-
-
-                </p>
-            </div>
-            <div className='contenedor2'>
-                
-                <p className='texto2'>
-                    HOTEL GATUNO
-                </p>
-            </div>
-
-            <div className='contenedor3'>
-                <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#83A0A7"><path d="M226.67-186.67h140v-246.66h226.66v246.66h140v-380L480-756.67l-253.33 190v380ZM160-120v-480l320-240 320 240v480H526.67v-246.67h-93.34V-120H160Zm320-352Z"/></svg>
-                    <button  className='btn' onClick={handleRegistro}>
-                            Dashboard
-                    </button>
-            </div>
-
-            <div className='contenedor3'>
-                {/* Icono */}
-            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#83A0A7"><path d="M226.67-186.67h140v-246.66h226.66v246.66h140v-380L480-756.67l-253.33 190v380ZM160-120v-480l320-240 320 240v480H526.67v-246.67h-93.34V-120H160Zm320-352Z"/></svg>
-                    <button  className='btn' onClick={RegistroDegatos}>
-                        Registro de gatos
-                    </button>
-
-            </div>
-
-            <div className='contenedor3'>
-
-            </div>
-
-            <div className='contenedor3'>
-
-            </div>
-
-            <div className='contenedor3'>
-
-            </div>
-            |
+  return (
+    <div className="contenedor-principal">
+      <div className="contenedor-iconos">
+        <div className="contenedor0">
+          <img src="src/assets/Vector 1.svg" alt="" />
         </div>
-
-
-
-
-
+        <div className="contenedor1">
+          <p className="titulo1">Peponé</p>
+        </div>
+        <div className="contenedor2">
+          <p className="texto2">HOTEL GATUNO</p>
+        </div>
+        <div className="contenedor">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#e3e3e3"
+          >
+            <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
+          </svg>
+          <button className="btn" onClick={handleRegistro}>
+            Dashboard
+          </button>
+        </div>
+        <div className="contenedor">
+          {/* Icono */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#e3e3e3"
+          >
+            <path d="M180-475q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29Zm180-160q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29Zm240 0q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29Zm180 160q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29ZM266-75q-45 0-75.5-34.5T160-191q0-52 35.5-91t70.5-77q29-31 50-67.5t50-68.5q22-26 51-43t63-17q34 0 63 16t51 42q28 32 49.5 69t50.5 69q35 38 70.5 77t35.5 91q0 47-30.5 81.5T694-75q-54 0-107-9t-107-9q-54 0-107 9t-107 9Z" />
+          </svg>
+          <button className="btn" onClick={RegistroDegatos}>
+            Registro de gatos
+          </button>
+        </div>
+        <div className="contenedor">
+          {/* Icono */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#e3e3e3"
+          >
+            <path d="M680-80q-83 0-141.5-58.5T480-280q0-83 58.5-141.5T680-480q83 0 141.5 58.5T880-280q0 83-58.5 141.5T680-80Zm67-105 28-28-75-75v-112h-40v128l87 87Zm-547 65q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h167q11-35 43-57.5t70-22.5q40 0 71.5 22.5T594-840h166q33 0 56.5 23.5T840-760v250q-18-13-38-22t-42-16v-212h-80v120H280v-120h-80v560h212q7 22 16 42t22 38H200Zm280-640q17 0 28.5-11.5T520-800q0-17-11.5-28.5T480-840q-17 0-28.5 11.5T440-800q0 17 11.5 28.5T480-760Z" />
+          </svg>
+          <button className="btn" onClick={Citas}>
+            Citas
+          </button>
+        </div>
+        <div className="contenedor">
+          {/* Icono */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#e3e3e3"
+          >
+            <path d="M160-80q-33 0-56.5-23.5T80-160v-480q0-33 23.5-56.5T160-720h160v-80q0-33 23.5-56.5T400-880h160q33 0 56.5 23.5T640-800v80h160q33 0 56.5 23.5T880-640v480q0 33-23.5 56.5T800-80H160Zm0-80h640v-480H160v480Zm240-560h160v-80H400v80ZM160-160v-480 480Zm280-200v120h80v-120h120v-80H520v-120h-80v120H320v80h120Z" />
+          </svg>
+          <button className="btn" onClick={Tratamientos}>
+            Tratamientos
+          </button>
+        </div>
+        <div className="contenedor">
+          {/* Icono */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#e3e3e3"
+          >
+            <path d="M240-400h320v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z" />
+          </svg>
+          <button className="btn" onClick={Mensajes}>
+            Mensajes
+          </button>
+        </div>
+        <div className="contenedor">
+          {/* Icono */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#e3e3e3"
+          >
+            <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
+          </svg>
+          <button className="btn" onClick={Cerrarsesión}>
+            Cerrar sesión
+          </button>
+        </div>
+        |
+      </div>
     </div>
+  );
+};
 
-
-    );
-}
-
-    
 export default Munu;
